1db48c5b28a5e4bb1ebd37ef3e2a09040826f291
Fabio Riva <f.riva@sotefin.ch>
2024-10-25 08:49:44 +0200

    removed unecessary p tag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,6 @@
 </script>
 
 <div class="font-medium space-y-32">
-	<p>Page is {page.nr}</p>
 	<video autoplay loop muted src={VIDEO_SRC} class="px-0"></video>
 	<p class="uppercase text-6xl">swipe tag to start</p>
 	<img src="finger-down-148.png" alt="finger-down" class="mx-auto" />
