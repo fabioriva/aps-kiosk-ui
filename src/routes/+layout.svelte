ceea4e7d9986ed25302e73d035fc5e7713bd1103
Fabio Riva <f.riva@sotefin.ch>
2024-10-25 11:19:39 +0200

    main page styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,6 +18,9 @@
 			page.nr = m['page'];
 		};
 	});
+// 	const heart = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+//   <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+// </svg>'
 </script>
 
 <div class="text-center text-xl">
@@ -32,14 +35,16 @@
 	</div>
 
 	<div class="flex flex-col h-screen">
-		<div class="m-auto bg-opacity-0">
+		<div class="m-auto bg-opacity-0 px-1">
 			{@render children()}
 		</div>
 	</div>
 
 	<div class="absolute bottom-0 flex justify-center items-center py-3 w-full">
 		<span class="font-medium"
-			>© {new Date().getFullYear()} Sotefin SA. Made with ❤️ in Switzerland.</span
+			>© {new Date().getFullYear()} Sotefin SA. Made with <span class="inline-block"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="rgb(239 68 68)" class="bi bi-heart-fill" viewBox="0 0 16 16">
+				<path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+			</svg></span> in Switzerland.</span
 		>
 	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,8 +7,16 @@
 	$inspect(page);
 </script>
 
-<div class="font-medium space-y-32">
+<div class="font-medium">
 	<video autoplay loop muted src={VIDEO_SRC} class="px-0"></video>
-	<p class="uppercase text-6xl">swipe tag to start</p>
-	<img src="finger-down-148.png" alt="finger-down" class="mx-auto" />
+	<div class="mt-16 space-y-16">
+	<div class="bg-zinc-100 border-t-4 border-zinc-700 rounded-b text-zinc-700 px-4 py-3 shadow-md" role="alert">
+		<p class="font-bold text-xl">Action required</p>
+		<p class="py-4 text-5xl uppercase">Swipe tag to start</p>
+	</div>
+	<!-- <img src="finger-down-148.png" alt="finger-down" class="mx-auto" /> -->
+	<svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" fill="currentColor" class="bi bi-arrow-down-circle mx-auto" viewBox="0 0 16 16">
+		<path fill="rgb(63 63 70)" fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z"/>
+	</svg>
+</div>
 </div>
