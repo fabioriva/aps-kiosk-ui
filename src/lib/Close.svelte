692bd94e1a4d31eec8bb616dc4ac7e07634124d2
Fabio Riva <f.riva@sotefin.ch>
2024-10-28 12:34:27 +0100

    new <Close /> component

---
+++ src/lib/Close.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Alert from '$lib/Alert.svelte';
+
+	let isPressed = $state(false);
+
+	const push = async () => {
+		isPressed = true;
+		await fetch('/api/push');
+	};
+	const release = async () => {
+		isPressed = false;
+		await fetch('/api/release');
+	};
+</script>
+
+<Alert message="Press & hold button to close" role="warning" title="Action required" />
+<div class="mt-24">
+	<button
+		class={`w-[256px] h-[256px] rounded-full bg-yellow-500 border-4 border-yellow-600 opacity-100 shadow-2xl shadow-slate-700 font-bold text-2xl text-amber-700 ${isPressed && 'bg-gradient-to-br from-amber-500 to-amber-600 text-amber-600'}`}
+		ontouchend={release}
+		onclick={push}
+		ontouchstart={push}>PUSH TO CLOSE</button
+	>
+</div>
