7a66c1db3fb9dbed95e57d957d3a28b6e646e73f
Fabio Riva <f.riva@sotefin.ch>
2024-11-05 09:46:52 +0100

    Key component styling

--- src/lib/Key.svelte
+++ src/lib/Key.svelte
@@ -10,6 +10,6 @@
 	class="{!isNaN(key)
 		? 'text-gray-700 border-gray-800 bg-gray-50 active:bg-gray-500'
 		: 'text-blue-700 border-blue-700 bg-blue-300 active:bg-blue-500'}
-    h-24 w-24 align-middle font-semibold text-6xl border-2 rounded-lg"
+    h-24 w-24 align-middle font-semibold text-6xl border-4 border-double rounded-lg"
 	ontouchstart={() => press(key)}>{key}</button
 >
