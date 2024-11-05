b7aa2898d067b252c0c71648788adcd2838bcd0f
Fabio Riva <f.riva@sotefin.ch>
2024-11-05 09:22:31 +0100

    remove unecessary div

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,24 +9,23 @@
 	$inspect(page);
 </script>
 
-<div class="font-medium">
-	<div class="mx-8 mt-16">
-		{#if page.nr === 1}
-			<Alert message="Swipe tag to start" role="info" title="Action required" />
-			<!-- <img src="arrow-down.png" alt="arrow-down" class="mx-auto mt-32" width="256" height="256" /> -->
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="256"
-				height="256"
-				fill="#0369a1"
-				class="bi bi-arrow-down-square-fill mx-auto mt-32"
-				viewBox="0 0 16 16"
-			>
-				<path
-					d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm6.5 4.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5a.5.5 0 0 1 1 0"
-				/>
-			</svg>
-			<!-- <svg
+<div class="font-medium mx-8 mt-16">
+	{#if page.nr === 1}
+		<Alert message="Swipe tag to start" role="info" title="Action required" />
+		<!-- <img src="arrow-down.png" alt="arrow-down" class="mx-auto mt-32" width="256" height="256" /> -->
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="256"
+			height="256"
+			fill="#0369a1"
+			class="bi bi-arrow-down-square-fill mx-auto mt-32"
+			viewBox="0 0 16 16"
+		>
+			<path
+				d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm6.5 4.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5a.5.5 0 0 1 1 0"
+			/>
+		</svg>
+		<!-- <svg
 				xmlns="http://www.w3.org/2000/svg"
 				width="256"
 				height="256"
@@ -39,17 +38,16 @@
 					d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z"
 				/>
 			</svg> -->
-		{:else if page.nr == 2}
-			<Close />
-		{:else if page.nr == 3}
-			<!-- <Alert message="Enter PIN code" role="warning" title="Action required" /> -->
-			<Keypad />
-		{:else if page.nr == 4}
-			<Success message="Success message" />
-		{:else if page.nr == 5}
-			<Error message="Error message" />
-		{:else}
-			<div>page {page.nr}</div>
-		{/if}
-	</div>
+	{:else if page.nr == 2}
+		<Close />
+	{:else if page.nr == 3}
+		<!-- <Alert message="Enter PIN code" role="warning" title="Action required" /> -->
+		<Keypad />
+	{:else if page.nr == 4}
+		<Success message="Success message" />
+	{:else if page.nr == 5}
+		<Error message="Error message" />
+	{:else}
+		<div>page {page.nr}</div>
+	{/if}
 </div>
